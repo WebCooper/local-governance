fb203b36ddb53e310acdc85cff05efe2d3e5dd03
nostoc <nostochk@gmail.com>
2026-07-02 20:59:12 +0530

    remove some super admins

--- smart-contracts/ignition/modules/Reporting.ts
+++ smart-contracts/ignition/modules/Reporting.ts
@@ -4,9 +4,9 @@ export default buildModule("ReportingAndMultiSig", (m) => {
   const reporting = m.contract("Reporting");
 
   const initialSuperAdmins = [
-    "0x51F279FC45e8bD2E66501391Df378Da045Cd3d45", // User's main wallet
-    "0x2f82af1eDf8ddA7C6f87AFdc1B60Dc5cA4C76B23", // Super Admin 2
-    "0xD019C08F95B5450F36Fcb8D1d4Ba8AB73B64fDA7", // Super Admin 3
+    // "0x51F279FC45e8bD2E66501391Df378Da045Cd3d45", // User's main wallet
+    // "0x2f82af1eDf8ddA7C6f87AFdc1B60Dc5cA4C76B23", // Super Admin 2
+    // "0xD019C08F95B5450F36Fcb8D1d4Ba8AB73B64fDA7", // Super Admin 3
     // //defaults hardhat users for test purposes
     "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", // 0
     "0x70997970c51812dc3a010c7d01b50e0d17dc79c8", // 1
@@ -14,7 +14,6 @@ export default buildModule("ReportingAndMultiSig", (m) => {
     "0x90f79bf6eb2c4f870365e785982e1f101e93b906", //3
     "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65",//4
     "0x9965507d1a55bcc2695c58ba16fb37d819b0a4dc" //5
-
   ];
 
   const authorityMultiSig = m.contract("AuthorityMultiSig", [initialSuperAdmins, reporting]);
